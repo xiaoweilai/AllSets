392028248885043c462a6b25ba46aaee2732ae8f
yjduoduo <wxjlmr@126.com>
2015-07-08 23:23:46 +0800

    手机页面的转换，setContentView的应用，但切换到另外一个界面后会死机，界面跳转错误，ok

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+//import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.content.res.Resources;
 //import android.graphics.Color;
@@ -25,7 +25,7 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     private Button mButton1;
     private Button mButton2;
-    private TextView mTextView1;
+    //    private TextView mTextView1;
     //    private TextView mTextView01;
 //    private TextView mTextView02;
     @Override
@@ -35,26 +35,56 @@ public class MainActivity extends Activity {
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.fragment_main);
 
-        mButton1 = (Button)findViewById(R.id.myButton1);
-        mButton2 = (Button)findViewById(R.id.myButton2);
-        mTextView1 = (TextView)findViewById(R.id.myTextView1);
+        /* 以findViewById()取得Button对象，并添加onClickListerner  */
+        mButton1 = (Button)findViewById(R.id.button1);
 
         mButton1.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v)
             {
-                mTextView1.setText("Hi, EveryOne!!");
+//                mTextView1.setText("Hi, EveryOne!!");
+                jumpToLayout2();
             }
 
         });
+
+    }
+
+    /* method jumpToLayout2:将layout由main.xml切换成mylayout.xml */
+    public void jumpToLayout2()
+    {
+        /* 将layout改成mylayout.xml */
+        setContentView(R.layout.mylayout);
+
+        /*  以findViewById()取得button对象，并添加onClickLister */
+        mButton2 = (Button)findViewById(R.id.button2);
+
 
         mButton2.setOnClickListener(new Button.OnClickListener()
         {
             @Override
             public void onClick(View view) {
-                mTextView1.setText("Hi,David!!!!!!");
+//                mTextView1.setText("Hi,David!!!!!!");
+                jumpToLayout1();
+
             }
         });
 
+    }
+
+    public void jumpToLayout1()
+    {
+        setContentView(R.layout.fragment_main);
+        /* 以findViewById()取得Button对象，并添加onClickListerner  */
+        mButton1 = (Button)findViewById(R.id.button1);
+
+        mButton1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+//                mTextView1.setText("Hi, EveryOne!!");
+                jumpToLayout2();
+            }
+        });
     }
 }

ae06fe5a80de92cda9240f35fb13a72fee78ef6b
yjduoduo <wxjlmr@126.com>
2015-07-08 22:54:23 +0800

    简易的按钮事件 Button事件处理

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -1,12 +1,15 @@
 package com.source.all;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+//import android.util.DisplayMetrics;
 //import android.content.res.Resources;
 //import android.graphics.Color;
 //import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.util.DisplayMetrics;
+
 
 //import android.support.v7.app.ActionBarActivity;
 //import android.support.v7.app.ActionBar;
@@ -14,13 +17,15 @@ import android.util.DisplayMetrics;
 //import android.view.LayoutInflater;
 //import android.view.Menu;
 //import android.view.MenuItem;
-//import android.view.View;
+
 //import android.view.ViewGroup;
 //import android.os.Build;
 
 
 public class MainActivity extends Activity {
-//    private TextView mTextView01;
+    private Button mButton1;
+    private TextView mTextView1;
+    //    private TextView mTextView01;
 //    private TextView mTextView02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +33,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.fragment_main);
+
+        mButton1 = (Button)findViewById(R.id.myButton1);
+        mTextView1 = (TextView)findViewById(R.id.myTextView1);
+
+        mButton1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                mTextView1.setText("Hi, EveryOne!!");
+            }
+
+        });
+
     }
 }

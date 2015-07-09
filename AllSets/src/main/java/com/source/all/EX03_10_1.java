f0d9b6e0428acc833c43d61484e26a348d8a9dee
yjduoduo <wxjlmr@126.com>
2015-07-10 00:35:23 +0800

    不同Activity之间的数据传递 Bundle对象的实现，点击按钮挂掉

---
+++ AllSets/src/main/java/com/source/all/EX03_10_1.java
@@ -0,0 +1,70 @@
+package com.source.all;
+
+/* import 相关class */
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by John on 15-7-10.
+ */
+public class EX03_10_1 extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /* 加载mylayout.xml */
+        setContentView(R.layout.mylayout);
+
+        /* 取得Intent中的Bundle对象 */
+        Bundle bundle = new Bundle();
+
+        /* 取得Bundle对象中的数据 */
+        double height = bundle.getDouble("height");
+        String sex = bundle.getString("sex");
+
+        /* 判断性别 */
+        String sexText = "";
+        if(sex.equals("M"))
+        {
+            sexText = "男性";
+        }
+        else
+        {
+            sexText = "女性";
+        }
+
+        /* 取得标准体重 */
+        String weight = this.getWeight(sex,height);
+
+        /* 设置输出文字 */
+        TextView tv1 = (TextView)findViewById(R.id.text1);
+        tv1.setText("你是一位" + sexText + "\n你的身高是"
+        + height + "厘米\n 你的标准体重是" + weight + "公斤");
+    }
+
+    /* 四舍五入的method */
+    private String format(double num)
+    {
+        NumberFormat formatter = new DecimalFormat("0.00");
+        String s = formatter.format(num);
+        return s;
+    }
+
+    /* 以findViewById()取得Button对象，并添加onClickListener */
+    private String getWeight(String sex, double height)
+    {
+        String weight = "";
+        if(sex.equals("M"))
+        {
+            weight = format((height - 80)*0.7);
+        }
+        else
+        {
+            weight = format((height - 70)*0.6);
+        }
+        return weight;
+    }
+
+}
--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.RadioButton;
 //import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.content.res.Resources;
@@ -43,55 +45,41 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v)
             {
+
+                /*  取得输入的身高 */
+                EditText et = (EditText)findViewById(R.id.height);
+                double height = Double.parseDouble(et.getText().toString());
+                /* 取得性别 */
+                String sex = "";
+                RadioButton rb1 = (RadioButton)findViewById(R.id.sex1);
+                if(rb1.isChecked())
+                {
+                    sex = "M";
+                }
+                else
+                {
+                    sex = "F";
+                }
+
                 /* new一个Intent对象，并指定要启动的class */
                 Intent intent = new Intent();
-                intent.setClass(MainActivity.this, EX03_09_1.class);
+                intent.setClass(MainActivity.this, EX03_10_1.class);
 
-                /* 调用一个新的Activity */
+                /* new一个Bundle对象，并将要传递的数据传入 */
+                Bundle bundle = new Bundle();
+                bundle.putDouble("height", height);
+                bundle.putString("sex", sex);
+
+                /* 将Bundle对象assign给Intent */
+                intent.putExtras(bundle);
+                /* 调用一个新的Activity  即EX03_10_1*/
                 startActivity(intent);
-                /* 关闭原本的 Activity */
-                MainActivity.this.finish();
+
+//                /* 关闭原本的 Activity */
+//                MainActivity.this.finish();
             }
 
         });
 
-    }
-
-    /* method jumpToLayout2:将layout由main.xml切换成mylayout.xml */
-    public void jumpToLayout2()
-    {
-        /* 将layout改成mylayout.xml */
-        setContentView(R.layout.mylayout);
-
-        /*  以findViewById()取得button对象，并添加onClickLister */
-        mButton2 = (Button)findViewById(R.id.button2);
-
-
-        mButton2.setOnClickListener(new Button.OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-//                mTextView1.setText("Hi,David!!!!!!");
-                jumpToLayout1();
-
-            }
-        });
-
-    }
-
-    public void jumpToLayout1()
-    {
-        setContentView(R.layout.fragment_main);
-        /* 以findViewById()取得Button对象，并添加onClickListerner  */
-        mButton1 = (Button)findViewById(R.id.button1);
-
-        mButton1.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v)
-            {
-//                mTextView1.setText("Hi, EveryOne!!");
-                jumpToLayout2();
-            }
-        });
     }
 }

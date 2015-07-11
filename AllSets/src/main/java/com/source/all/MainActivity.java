8c311073b78f703dcad6247bffbe2ffe31f84d2f
yjduoduo <wxjlmr@126.com>
2015-07-11 15:25:49 +0800

    今晚到哪儿打牙祭 具有选择功能的对话框，但挂死

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -2,7 +2,9 @@ package com.source.all;
 
 //import com.source.all.R;
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+//import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,67 +40,52 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-//    private Button mButton1;
-//    private TextView mTextView1;
+    private Button mButton1;
+    private TextView mTextView1;
     /* 创建一个全局的类成员变量，类型为ProgressDialog对象 */
-    public ProgressDialog myDialog = null;
+//    public ProgressDialog myDialog = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         /* Called when the activity is first created. */
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-//        setContentView(R.layout.fragment_main);
+        setContentView(R.layout.fragment_main);
 
-        Button btnButton1 = new Button(this);
-        this.setContentView(btnButton1); //按钮布满全屏幕
-        btnButton1.setText(R.string.str_btn1);
-
-        /* 为创建好的按钮对象，指定OnClickListerner
-        * 即单击按钮会运行的事件
-        * 并在事件处理方法中显示ProgressBar */
-
-        btnButton1.setOnClickListener(myShowPorgressBar);
+        mButton1 = (Button)findViewById(R.id.myButton1);
+        mTextView1 = (TextView)findViewById(R.id.myTextView1);
+        mButton1.setOnClickListener(myShowAlertDialog);
     }
 
     /* 单击按钮运行的OnClickListener事件方法 */
-    Button.OnClickListener myShowPorgressBar =
+    Button.OnClickListener myShowAlertDialog =
     new Button.OnClickListener()
     {
         @Override
         public void onClick(View view) {
-            final CharSequence strDialogTitle = getString(R.string.str_dialog_title);
-            final CharSequence strDialogBody = getString(R.string.str_dialog_body);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle(R.string.str_alert_title) //设置标题
+                    .setItems(R.string.str_alert_body,
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    CharSequence strDialogBody = getString(R.string.str_alert_body);
 
-                /* 显示Progress对话框 */
-            myDialog = ProgressDialog.show(
-                    MainActivity.this,
-                    strDialogTitle,
-                    strDialogBody,
-                    true
-            );
+                                    String[] aryShop = getResources().getStringArray(R.array.items_irdc_dialog);
+                                    new AlertDialog.Builder(MainActivity.this)
+                                            .setMessage(strDialogBody + aryShop[i])
+                                            .setNeutralButton(R.string.str_ok,
+                                                    new DialogInterface.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                                            /* 在这里处理要做的事 */
+                                                        }
+                                                    }).show();
 
-            new Thread()
-            {
-                public void run()
-                {
-                    try
-                    {
-                            /* 在这里写上要后台运行的代码段
-                            * 为了明显看见效果，以暂停3秒作为示范 */
-                        sleep(3000);
-                    }
-                    catch(Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-                    finally {
-                            /*卸载所创建的myDialog对象 */
-                        myDialog.dismiss();
-                    }
-                }
-            }.start();   /* 开始运行线程 */
-        } /* End:public void onClick(View arg0) */
+                                }
+                            }
+                    ).show();
+        }
     };
 
 }

cd7f9a34b3592e226e6e570db507e87cd3911e10
yjduoduo <wxjlmr@126.com>
2015-07-11 15:52:18 +0800

    android变脸 主题(theme)实现

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -2,13 +2,13 @@ package com.source.all;
 
 //import com.source.all.R;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+//import android.app.AlertDialog;
+//import android.content.DialogInterface;
 //import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+//import android.view.View;
+//import android.widget.Button;
+//import android.widget.TextView;
 //import android.widget.EditText;
 ///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
 //import android.content.Context;
@@ -19,12 +19,10 @@ import android.widget.TextView;
 //import android.widget.ImageSwitcher;
 
 //import android.content.Intent;
-//import android.widget.EditText;
 //import android.widget.RadioButton;
 //import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.content.res.Resources;
-//import android.graphics.Color;
 //import android.graphics.drawable.Drawable;
 
 
@@ -40,8 +38,8 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-    private Button mButton1;
-    private TextView mTextView1;
+//    private Button mButton1;
+//    private TextView mTextView1;
     /* 创建一个全局的类成员变量，类型为ProgressDialog对象 */
 //    public ProgressDialog myDialog = null;
 
@@ -50,58 +48,22 @@ public class MainActivity extends Activity {
         /* Called when the activity is first created. */
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
+
+        /* * 套用透明背景的主题 **/
+        setTheme(R.style.Theme_Transparent);
+
+        /* * 套用布景主题1 * */
+        setTheme(R.style.Theme_Translucent);
+
+        /* * 套用布景主题2 */
+        setTheme(R.style.Theme_Translucent2);
+
+
         setContentView(R.layout.fragment_main);
 
-        mButton1 = (Button)findViewById(R.id.myButton1);
-        mTextView1 = (TextView)findViewById(R.id.myTextView1);
-        mButton1.setOnClickListener(myShowAlertDialog);
+//        mButton1 = (Button)findViewById(R.id.myButton1);
+//        mTextView1 = (TextView)findViewById(R.id.myTextView1);
+//        mButton1.setOnClickListener(myShowAlertDialog);
     }
-
-    /* 单击按钮运行的OnClickListener事件方法 */
-    Button.OnClickListener myShowAlertDialog =
-    new Button.OnClickListener()
-    {
-        @Override
-        public void onClick(View view) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle(R.string.str_alert_title) //设置标题
-                    .setItems(R.array.items_irdc_dialog,
-                    new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialogInterface, int i) {
-                            CharSequence strDialogBody = getString(R.string.str_alert_body);
-
-                            String[] aryShop = getResources().getStringArray(R.array.items_irdc_dialog);
-                            new AlertDialog.Builder(MainActivity.this)
-                                    .setMessage(strDialogBody + aryShop[i])
-                                    .setNeutralButton(R.string.str_ok,
-                                            new DialogInterface.OnClickListener()
-                                    {
-                                        @Override
-                                        public void onClick(DialogInterface dialogInterface, int i) {
-                                            /* 在这里处理要做的事 */
-                                        }
-                                    })
-                                    .setNegativeButton("取消吗",
-                                            new DialogInterface.OnClickListener() {
-                                                @Override
-                                                public void onClick(DialogInterface dialogInterface, int i) {
-                                                    dialogInterface.dismiss();
-                                                }
-                                            })
-                                    .show();
-                        }
-                    }) //以.show()前面的)为分隔符
-                    //在第二层AlertDialog里显示取消按钮
-                    .setNegativeButton("取消",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialogInterface, int i) {
-                                    dialogInterface.dismiss();
-                                }
-                            })
-                    .show();
-        } /* End: public void onClick(View arg0) */
-    };
 
 }

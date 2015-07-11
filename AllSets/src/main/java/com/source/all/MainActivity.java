cb9fb3beed3a0986e420828fc88b8df2bd8dfef8
yjduoduo <wxjlmr@126.com>
2015-07-11 14:01:05 +0800

    自制计算器   多按钮的整合应用

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -1,18 +1,19 @@
 package com.source.all;
 
+import com.source.all.R;
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-/* 欲在Layout里使用Gallery widget，必须引用这些模块 */
-import android.content.Context;
-import android.widget.Gallery;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
-import android.widget.ImageSwitcher;
+import android.widget.EditText;
+///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
+//import android.content.Context;
+//import android.widget.Gallery;
+//import android.view.ViewGroup;
+//import android.widget.BaseAdapter;
+//import android.widget.ImageView;
+//import android.widget.ImageSwitcher;
 
 //import android.content.Intent;
 //import android.widget.EditText;
@@ -36,7 +37,15 @@ import android.widget.ImageSwitcher;
 
 
 public class MainActivity extends Activity {
-    private TextView mTextView01;
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButton4;
+    private Button mButton5;
+    private Button mButton6;
+    private EditText mEditText1;
+    private EditText mEditText2;
+    private TextView mTextView2;
+    private TextView mTextView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,63 +54,71 @@ public class MainActivity extends Activity {
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.fragment_main);
 
-        /* 以findViewById()取得TextView对象  */
-        mTextView01 = (TextView)findViewById(R.id.myTextView01);
-        mTextView01.setText(getString(R.string.str_txt1));
-        mTextView01.setTextColor(Color.BLUE);
+        /* 以findViewById()取得所部署的Layout对象  */
+        mTextView2 = (TextView)findViewById(R.id.mTextView2);
+        mTextView4 = (TextView)findViewById(R.id.mTextView4);
+        mButton2 = (Button)findViewById(R.id.mButton2);
+        mButton3 = (Button)findViewById(R.id.mButton3);
+        mButton4 = (Button)findViewById(R.id.mButton4);
+        mButton5 = (Button)findViewById(R.id.mButton5);
+        mEditText1 = (EditText)findViewById(R.id.mText1);
+        mEditText2 = (EditText)findViewById(R.id.mText2);
 
-        ((Gallery)findViewById(R.id.myGallery1)).setAdapter(new ImageAdapter(this) );
+        //+
+        mButton2.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                mTextView2.setText("+");
+                String strRet = Integer.toString(Integer.parseInt(mEditText1.getText().toString()) +
+                Integer.parseInt(mEditText2.getText().toString()));
+                mTextView4.setText(strRet);
+            }
+        });
+
+        //-
+        mButton3.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                mTextView2.setText("-");
+                String strRet = Integer.toString(Integer.parseInt(mEditText1.getText().toString())
+                        -
+                        Integer.parseInt(mEditText2.getText().toString()));
+                mTextView4.setText(strRet);
+            }
+        });
 
 
+        //*
+        mButton4.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                mTextView2.setText("*");
+                String strRet = Integer.toString(Integer.parseInt(mEditText1.getText().toString())
+                        *
+                        Integer.parseInt(mEditText2.getText().toString()));
+                mTextView4.setText(strRet);
+            }
+        });
+
+
+        // /
+        mButton5.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                mTextView2.setText("/");
+                String strRet = Integer.toString(Integer.parseInt(mEditText1.getText().toString())
+                        /
+                        Integer.parseInt(mEditText2.getText().toString()));
+                mTextView4.setText(strRet);
+            }
+        });
     }
 
-    /**
-     * Created by John on 15-7-11.
-     */
-    public class ImageAdapter extends BaseAdapter {
-        /* 类成员myContext为Context父类 */
-        private Context myContext;
 
-        /* 使用Android.R.drawable里的图片作为图库集，类型为整数数组 */
-        private int[] myImageIds =
-                {
-                        android.R.drawable.btn_minus,
-                        android.R.drawable.btn_radio,
-                        android.R.drawable.ic_lock_idle_low_battery,
-                        android.R.drawable.ic_menu_camera
-                };
-        /* 构造器只有一个参数，即要存储的Context */
-        public ImageAdapter(Context c){this.myContext = c;}
-
-        /* 返回所有已定义的图片总数量 */
-        public int getCount() {return this.myImageIds.length;}
-
-        /* 利用getItem方法，取得目前容器中图像的数组ID */
-        public Object getItem(int position) {return position;}
-        public long getItemId(int position) {return position;}
-
-        /* 取得目前令人欲显示的图像View,传入数组ID值使之读取与成像 */
-        public View getView(int position, View covertView,
-                            ViewGroup parent)
-        {
-            /* 创建一个ImageView对象 */
-            ImageView i = new ImageView(this.myContext);
-
-            i.setImageResource(this.myImageIds[position]);
-            i.setScaleType(ImageView.ScaleType.FIT_XY);
-
-            /* 设置这个ImageView对象的宽高，单位为dip */
-            i.setLayoutParams(new Gallery.LayoutParams(120,120));
-            return i;
-        }
-
-        /* 依据距离中央的位移量 利用getScale返回Views的大小(0.0f to 1.0f) */
-        public float getScale(boolean focused, int offset)
-        {
-            /* Formula:1 / (2 ^ offset) */
-            return Math.max(0, 1.0f/(float)Math.pow(2, Math.abs(offset)));
-        }
-    }
 
 
 }

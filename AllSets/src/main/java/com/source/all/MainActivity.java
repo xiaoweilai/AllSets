1581f1e15fb740b6f7603e32dd4773f3cf8d01a9
yjduoduo <wxjlmr@126.com>
2015-07-22 22:14:19 +0800

    设计具有背景图的按钮  ImageButton的焦点及事件处理

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -2,7 +2,7 @@ package com.source.all;
 
 //import com.source.all.R;
 import android.app.Activity;
-import android.view.KeyEvent;
+//import android.view.KeyEvent;
 ///* 打勾显示输入的密码  语系*/
 //import android.content.res.Configuration;
 //import android.util.DisplayMetrics;
@@ -13,11 +13,15 @@ import android.view.KeyEvent;
 //import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
-//import android.widget.Button;
-//import android.widget.TextView;
+/* 使用OnClickListener与OnFocusChangeListener来区分按钮的状态 */
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.widget.CompoundButton;
-import android.widget.EditText;
+//import android.widget.EditText;
 //import android.widget.CheckBox;
 ///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
 //import android.content.Context;
@@ -29,7 +33,7 @@ import android.widget.EditText;
 
 //import android.content.Intent;
 //import android.widget.RadioButton;
-import android.widget.TextView;
+//import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.content.res.Resources;
 
@@ -49,12 +53,13 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-//    private Button mButton1;
-//    private TextView mTextView1;
+    private Button mButton1;
+    private TextView mTextView1;
+    private ImageButton mImageButton1;
 //    private EditText et;
 //    private CheckBox cb;
-    private EditText mEditText01;
-    private TextView mTextView01;
+//    private EditText mEditText01;
+//    private TextView mTextView01;
 
 
     /* 创建一个全局的类成员变量，类型为ProgressDialog对象 */
@@ -67,21 +72,56 @@ public class MainActivity extends Activity {
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.fragment_main);
 
-        /*取得TextView、EditText*/
-        mTextView01 = (TextView)findViewById(R.id.myTextView);
-        mEditText01 = (EditText)findViewById(R.id.myEditText);
+        /*  透过findViewById建构三个对象 */
+        mImageButton1 = (ImageButton)findViewById(R.id.myImageButton1);
+        mButton1 = (Button)findViewById(R.id.myButton1);
+        mTextView1 = (TextView)findViewById(R.id.myTextView1);
 
-        /*设定EditText用OnKeyListener事件来启动*/
-        mEditText01.setOnKeyListener(new EditText.OnKeyListener()
-        {
+        /* 透过OnFocusChangeListener来响应ImageButton的Focus事件  */
+        mImageButton1.setOnFocusChangeListener(new OnFocusChangeListener() {
             @Override
-            public boolean onKey(View view, int i, KeyEvent keyEvent) {
-                // TODO Auto-generated method stub
-                /*设定TextView显示EditText所输入的内容*/
-                mTextView01.setText(mEditText01.getText());
-                return false;
+            public void onFocusChange(View view, boolean b) {
+                //TODO Auto-generated method stub
+                /* 若ImageButton状态为OnFocus改变ImageButton的图片 * 并改变TextView的文字 */
+                if(b)
+                {
+                    mTextView1.setText("图片按钮状态为:Got Focus");
+                    mImageButton1.setImageResource(R.drawable.iconfull);
+                }
+                /* 若ImageButotn状态为offFocus改变ImageButton的图片 * 并改变textView的文字 */
+                else
+                {
+                    mTextView1.setText("图片按钮状态为：Lost Focus");
+                    mImageButton1.setImageResource(R.drawable.iconempty);
+                }
             }
         });
+        /* 透过OnClickListener来响应ImageButton的onClick事件  */
+        mImageButton1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Auto-generated method stub
+                /* 若ImageButton状态为onClick改变ImageButton的图片 * 并改变textView的文字 */
+                mTextView1.setText("图片按钮状态为：Got Click");
+                mImageButton1.setImageResource(R.drawable.iconfull);
+            }
+        });
+
+        /* 透过OnClickLister来响应Button的onClick事件 */
+        mButton1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO Auto-generated method stub
+                /* 若Button状态为onClick改变ImageButton的图片 * 并改变textView的文字 */
+                mTextView1.setText("图片按钮状态为：Lost Focus");
+                mImageButton1.setImageResource(R.drawable.iconempty);
+            }
+        });
+
+
+
+
+
     }
 
 }

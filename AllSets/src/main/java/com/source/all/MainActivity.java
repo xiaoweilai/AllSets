b56be9ff4bd62ba88f57da53a62ff6771ecf3bef
yjduoduo <wxjlmr@126.com>
2015-07-22 23:32:24 +0800

    我同意条款 CheckBox的isChecked属性

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -12,19 +12,19 @@ import android.app.Activity;
 //import android.content.DialogInterface;
 //import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.text.Editable;
+//import android.text.Editable;
 import android.view.View;
 /* 使用OnClickListener与OnFocusChangeListener来区分按钮的状态 */
-import android.view.View.OnClickListener;
+//import android.view.View.OnClickListener;
 //import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 //import android.widget.ImageButton;
 import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.Toast;
-//import android.widget.CheckBox;
+//import android.widget.EditText;
+//import android.widget.Toast;
+import android.widget.CheckBox;
 ///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
 //import android.content.Context;
 //import android.widget.Gallery;
@@ -55,9 +55,15 @@ import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+    /* 声明TextView、CheckBox、Button对象 */
+    private Button myButton;
+    private TextView myTextView1;
+    private TextView myTextView2;
+    private CheckBox myCheckBox;
+
+//    private EditText mEditText;
 //    private Button mButton1;
-    private Button mButton;
-    private EditText mEditText;
+
 //    private TextView mTextView1;
 //    private ImageButton mImageButton1;
 //    private EditText et;
@@ -76,25 +82,54 @@ public class MainActivity extends Activity {
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.fragment_main);
 
-        /*  透过findViewById建构三个对象 */
-        mButton = (Button)findViewById(R.id.myButton);
-        mEditText = (EditText)findViewById(R.id.myEditText);
+        /*  透过findViewById建构对象 ,取得TextView,CheckBox,Button */
+        myTextView1 = (TextView)findViewById(R.id.myTextView1);
+        myTextView2 = (TextView)findViewById(R.id.myTextView2);
+        myCheckBox = (CheckBox)findViewById(R.id.myCheckBox);
+        myButton = (Button)findViewById(R.id.myButton);
+//        mEditText = (EditText)findViewById(R.id.myEditText);
+        /* 将CheckBox、Button预设为未选取状态 */
+        myCheckBox.setChecked(false);
+        myButton.setEnabled(false);
 
-        /* 透过OnClickLister来响应Button的onClick事件 */
-        mButton.setOnClickListener(new OnClickListener() {
+
+        myCheckBox.setOnClickListener(new CheckBox.OnClickListener()
+
+        {
             @Override
             public void onClick(View view) {
-                //TODO Auto-generated method stub
-                /* 声明字符串变量并取得用户输入的EditText字符串 */
-                Editable str;
-                str = mEditText.getText();
-                /* 使用系统标准的makeText()方式来产生Toast讯息 */
-                Toast.makeText(MainActivity.this, "你的愿望 " + str.toString() + "已送达耶稣圣诞老人信箱", Toast.LENGTH_LONG).show();
-                /* 清空EditText */
-                mEditText.setText("");
+                //TODO Auto_generated method stub
+                if(myCheckBox.isChecked())
+                {
+                    myButton.setEnabled(true);
+                    myTextView2.setText("");
+                }
+                else
+                {
+                    myButton.setEnabled(false);
+                    myTextView1.setText(R.string.text1);
+                    /* 在TextView2里显示出“请勾选我同意” */
+                    myTextView2.setText(R.string.no);
+                }
             }
         });
 
+
+        myButton.setOnClickListener(new Button.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                //TODO Auto-generated method stub
+                if(myCheckBox.isChecked())
+                {
+                    myTextView1.setText(R.string.ok);
+                }
+                else
+                {
+
+                }
+            }
+        });
 
 
 

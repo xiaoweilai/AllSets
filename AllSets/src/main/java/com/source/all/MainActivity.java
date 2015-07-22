98c4e5aab6372887b768a97e0313b18ba75b3bce
yjduoduo <wxjlmr@126.com>
2015-07-22 23:58:01 +0800

    消费券采购列表   多选项多选项CheckBox的应用

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -13,11 +13,11 @@ import android.app.Activity;
 //import android.app.ProgressDialog;
 import android.os.Bundle;
 //import android.text.Editable;
-import android.view.View;
+//import android.view.View;
 /* 使用OnClickListener与OnFocusChangeListener来区分按钮的状态 */
 //import android.view.View.OnClickListener;
 //import android.view.View.OnFocusChangeListener;
-import android.widget.Button;
+//import android.widget.Button;
 //import android.widget.ImageButton;
 import android.widget.TextView;
 //import android.util.DisplayMetrics;
@@ -25,6 +25,7 @@ import android.widget.TextView;
 //import android.widget.EditText;
 //import android.widget.Toast;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 ///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
 //import android.content.Context;
 //import android.widget.Gallery;
@@ -55,11 +56,13 @@ import android.widget.CheckBox;
 
 
 public class MainActivity extends Activity {
-    /* 声明TextView、CheckBox、Button对象 */
-    private Button myButton;
-    private TextView myTextView1;
-    private TextView myTextView2;
-    private CheckBox myCheckBox;
+    /* 声明TextView、CheckBox对象 */
+//    private Button myButton;
+    private TextView mTextView1;
+//    private TextView myTextView2;
+    private CheckBox mCheckBox1;
+    private CheckBox mCheckBox2;
+    private CheckBox mCheckBox3;
 
 //    private EditText mEditText;
 //    private Button mButton1;
@@ -82,58 +85,66 @@ public class MainActivity extends Activity {
 //        setContentView(R.layout.activity_main);
         setContentView(R.layout.fragment_main);
 
-        /*  透过findViewById建构对象 ,取得TextView,CheckBox,Button */
-        myTextView1 = (TextView)findViewById(R.id.myTextView1);
-        myTextView2 = (TextView)findViewById(R.id.myTextView2);
-        myCheckBox = (CheckBox)findViewById(R.id.myCheckBox);
-        myButton = (Button)findViewById(R.id.myButton);
-//        mEditText = (EditText)findViewById(R.id.myEditText);
-        /* 将CheckBox、Button预设为未选取状态 */
-        myCheckBox.setChecked(false);
-        myButton.setEnabled(false);
-
-
-        myCheckBox.setOnClickListener(new CheckBox.OnClickListener()
-
-        {
-            @Override
-            public void onClick(View view) {
-                //TODO Auto_generated method stub
-                if(myCheckBox.isChecked())
-                {
-                    myButton.setEnabled(true);
-                    myTextView2.setText("");
-                }
-                else
-                {
-                    myButton.setEnabled(false);
-                    myTextView1.setText(R.string.text1);
-                    /* 在TextView2里显示出“请勾选我同意” */
-                    myTextView2.setText(R.string.no);
-                }
-            }
-        });
-
-
-        myButton.setOnClickListener(new Button.OnClickListener()
-        {
-            @Override
-            public void onClick(View view) {
-                //TODO Auto-generated method stub
-                if(myCheckBox.isChecked())
-                {
-                    myTextView1.setText(R.string.ok);
-                }
-                else
-                {
-
-                }
-            }
-        });
-
-
-
+        /*  透过findViewById建构对象 ,取得TextView对象并调整文字内容 */
+        mTextView1 = (TextView)findViewById(R.id.myTextView1);
+        mTextView1.setText("你所选择的项目有：");
+        /* 透过findViewById取得三个CheckBox对象 */
+        mCheckBox1 = (CheckBox)findViewById(R.id.myCheckBox1);
+        mCheckBox2 = (CheckBox)findViewById(R.id.myCheckBox2);
+        mCheckBox3 = (CheckBox)findViewById(R.id.myCheckBox3);
+        /* 设定OnCheckedChangeListener给三个CheckBox对象 */
+        mCheckBox1.setOnCheckedChangeListener(mCheckBoxChanged);
+        mCheckBox2.setOnCheckedChangeListener(mCheckBoxChanged);
+        mCheckBox3.setOnCheckedChangeListener(mCheckBoxChanged);
 
     }
+    /* 声明并建构OnCheckedChangeListener对象 */
+    private CheckBox.OnCheckedChangeListener mCheckBoxChanged =
+            new CheckBox.OnCheckedChangeListener()
+            {
+                @Override
+                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                    /* 透过getString()取得CheckBox的文字字符串 */
+                    String str0 = "所选的项目为： ";
+                    String str1 = getString(R.string.str_checkbox1);
+                    String str2 = getString(R.string.str_checkbox2);
+                    String str3 = getString(R.string.str_checkbox3);
+                    String plus = ";";
+                    String result = "但是超过预算啰！！";
+                    String result2 = "还可以再多买账本喔！！";
+                    /* 任一CheckBox被勾选后，该CheckBox的文字会改变TextView的文字内容 * 三个对象总共八种 情境 */
+                    if(mCheckBox1.isChecked() & mCheckBox2.isChecked() & mCheckBox3.isChecked())
+                    {
+                        mTextView1.setText(str0+str1+plus+str2+plus+str3+result);
+                    }
+                    else if(mCheckBox1.isChecked() & mCheckBox2.isChecked() & mCheckBox3.isChecked())
+                    {
+                        mTextView1.setText(str0+str2+plus+str3+result);
+                    }
+                    else  if(mCheckBox1.isChecked()==true & mCheckBox2.isChecked()==true & mCheckBox3.isChecked()==false)
+                    {
+                        mTextView1.setText(str0+str1+plus+str2+result); }
+                    else if(mCheckBox1.isChecked()==false & mCheckBox2.isChecked()==false & mCheckBox3.isChecked()==true)
+                    {
+                        mTextView1.setText(str0+str3+plus+result2);
+                    }
+                    else if(mCheckBox1.isChecked()==false & mCheckBox2.isChecked()==true & mCheckBox3.isChecked()==false)
+                    {
+                        mTextView1.setText(str0+str2);
+                    }
+                    else if(mCheckBox1.isChecked()==true & mCheckBox2.isChecked()==false & mCheckBox3.isChecked()==false)
+                    {
+                        mTextView1.setText(str0+str1);
+                    }
+                    else if(mCheckBox1.isChecked()==false & mCheckBox2.isChecked()==false & mCheckBox3.isChecked()==false)
+                    {
+                        mTextView1.setText(str0);
+                    }
+
+                }
+            };
+
+
+
 
 }

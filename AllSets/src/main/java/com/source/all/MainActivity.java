74ee647491e69417ccf81a444e4ab53f7c0aa78e
yjduoduo <wxjlmr@126.com>
2015-07-22 22:42:51 +0800

    给圣诞老人的信息 Toast对象的使用

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -12,16 +12,18 @@ import android.app.Activity;
 //import android.content.DialogInterface;
 //import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 /* 使用OnClickListener与OnFocusChangeListener来区分按钮的状态 */
 import android.view.View.OnClickListener;
-import android.view.View.OnFocusChangeListener;
+//import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
-import android.widget.ImageButton;
+//import android.widget.ImageButton;
 import android.widget.TextView;
 //import android.util.DisplayMetrics;
 //import android.widget.CompoundButton;
-//import android.widget.EditText;
+import android.widget.EditText;
+import android.widget.Toast;
 //import android.widget.CheckBox;
 ///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
 //import android.content.Context;
@@ -53,9 +55,11 @@ import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-    private Button mButton1;
-    private TextView mTextView1;
-    private ImageButton mImageButton1;
+//    private Button mButton1;
+    private Button mButton;
+    private EditText mEditText;
+//    private TextView mTextView1;
+//    private ImageButton mImageButton1;
 //    private EditText et;
 //    private CheckBox cb;
 //    private EditText mEditText01;
@@ -73,48 +77,21 @@ public class MainActivity extends Activity {
         setContentView(R.layout.fragment_main);
 
         /*  透过findViewById建构三个对象 */
-        mImageButton1 = (ImageButton)findViewById(R.id.myImageButton1);
-        mButton1 = (Button)findViewById(R.id.myButton1);
-        mTextView1 = (TextView)findViewById(R.id.myTextView1);
-
-        /* 透过OnFocusChangeListener来响应ImageButton的Focus事件  */
-        mImageButton1.setOnFocusChangeListener(new OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View view, boolean b) {
-                //TODO Auto-generated method stub
-                /* 若ImageButton状态为OnFocus改变ImageButton的图片 * 并改变TextView的文字 */
-                if(b)
-                {
-                    mTextView1.setText("图片按钮状态为:Got Focus");
-                    mImageButton1.setImageResource(R.drawable.iconfull);
-                }
-                /* 若ImageButotn状态为offFocus改变ImageButton的图片 * 并改变textView的文字 */
-                else
-                {
-                    mTextView1.setText("图片按钮状态为：Lost Focus");
-                    mImageButton1.setImageResource(R.drawable.iconempty);
-                }
-            }
-        });
-        /* 透过OnClickListener来响应ImageButton的onClick事件  */
-        mImageButton1.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO Auto-generated method stub
-                /* 若ImageButton状态为onClick改变ImageButton的图片 * 并改变textView的文字 */
-                mTextView1.setText("图片按钮状态为：Got Click！！！！");
-                mImageButton1.setImageResource(R.drawable.iconfull);
-            }
-        });
+        mButton = (Button)findViewById(R.id.myButton);
+        mEditText = (EditText)findViewById(R.id.myEditText);
 
         /* 透过OnClickLister来响应Button的onClick事件 */
-        mButton1.setOnClickListener(new OnClickListener() {
+        mButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 //TODO Auto-generated method stub
-                /* 若Button状态为onClick改变ImageButton的图片 * 并改变textView的文字 */
-                mTextView1.setText("图片按钮状态为：Lost Focus！！！");
-                mImageButton1.setImageResource(R.drawable.iconempty);
+                /* 声明字符串变量并取得用户输入的EditText字符串 */
+                Editable str;
+                str = mEditText.getText();
+                /* 使用系统标准的makeText()方式来产生Toast讯息 */
+                Toast.makeText(MainActivity.this, "你的愿望 " + str.toString() + "已送达耶稣圣诞老人信箱", Toast.LENGTH_LONG).show();
+                /* 清空EditText */
+                mEditText.setText("");
             }
         });
 

f6f30f3490a0b4ab61e6db1d4ab82b2f1dd30fd4
yjduoduo <wxjlmr@126.com>
2015-07-23 23:42:44 +0800

    向左或向右  RadioGroup组与onCheckedChanged事件

--- AllSets/src/main/java/com/source/all/MainActivity.java
+++ AllSets/src/main/java/com/source/all/MainActivity.java
@@ -19,15 +19,19 @@ import android.os.Bundle;
 //import android.view.View.OnFocusChangeListener;
 //import android.widget.Button;
 //import android.widget.ImageButton;
-import android.view.MotionEvent;
-import android.view.View;
+//import android.view.MotionEvent;
+//import android.view.View;
 import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 //import android.util.DisplayMetrics;
 //import android.widget.CompoundButton;
 //import android.widget.EditText;
 //import android.widget.Toast;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
+//import android.widget.CheckBox;
+//import android.widget.CompoundButton;
 ///* 欲在Layout里使用Gallery widget，必须引用这些模块 */
 //import android.content.Context;
 //import android.widget.Gallery;
@@ -61,10 +65,13 @@ public class MainActivity extends Activity {
     /* 声明TextView、CheckBox对象 */
 //    private Button myButton;
     private TextView mTextView1;
+    private RadioGroup mRadioGroup1;
+    private RadioButton mRadio1,mRadio2;
+    ArrayAdapter adapter2;
 //    private TextView myTextView2;
-    private CheckBox mCheckBox1;
-    private CheckBox mCheckBox2;
-    private CheckBox mCheckBox3;
+//    private CheckBox mCheckBox1;
+//    private CheckBox mCheckBox2;
+//    private CheckBox mCheckBox3;
 
 //    private EditText mEditText;
 //    private Button mButton1;
@@ -88,81 +95,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.fragment_main);
 
         /*  透过findViewById建构对象 ,取得TextView对象并调整文字内容 */
-        mTextView1 = (TextView)findViewById(R.id.myTextView1);
-        mTextView1.setText("你所选择的项目有：");
-        /* 透过findViewById取得三个CheckBox对象 */
-        mCheckBox1 = (CheckBox)findViewById(R.id.myCheckBox1);
-        mCheckBox2 = (CheckBox)findViewById(R.id.myCheckBox2);
-        mCheckBox3 = (CheckBox)findViewById(R.id.myCheckBox3);
-        /* 设定OnCheckedChangeListener给三个CheckBox对象 */
-        mCheckBox1.setOnCheckedChangeListener(mCheckBoxChanged);
-        mCheckBox2.setOnCheckedChangeListener(mCheckBoxChanged);
-        mCheckBox3.setOnCheckedChangeListener(mCheckBoxChanged);
+        mTextView1 = (TextView)findViewById(R.id.myTextView);
+        mRadioGroup1 = (RadioGroup)findViewById(R.id.myRadioGroup);
+        mRadio1 = (RadioButton)findViewById(R.id.myRadioButton1);
+        mRadio2 = (RadioButton)findViewById(R.id.myRadioButton2);
+        /* RadioGroup用OnCheckedChangeListener来执行 */
+        mRadioGroup1.setOnCheckedChangeListener(mChangeRadio);
 
     }
     /* 声明并建构OnCheckedChangeListener对象 */
-    private CheckBox.OnCheckedChangeListener mCheckBoxChanged =
-            new CheckBox.OnCheckedChangeListener()
-            {
+   private RadioGroup.OnCheckedChangeListener mChangeRadio =
+            new RadioGroup.OnCheckedChangeListener() {
                 @Override
-                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    /* 透过getString()取得CheckBox的文字字符串 */
-                    String str0 = "所选的项目为： ";
-                    String str1 = getString(R.string.str_checkbox1);
-                    String str2 = getString(R.string.str_checkbox2);
-                    String str3 = getString(R.string.str_checkbox3);
-                    String plus = ";";
-                    String result = "但是超过预算啰！！";
-                    String result2 = "还可以再多买账本喔！！";
-                    /* 任一CheckBox被勾选后，该CheckBox的文字会改变TextView的文字内容 * 三个对象总共八种 情境 */
-                    if(mCheckBox1.isChecked() & mCheckBox2.isChecked() & mCheckBox3.isChecked())
+                public void onCheckedChanged(RadioGroup radioGroup, int i) {
+//                    TODO Auto-generated method stub
+                    if(i == mRadio1.getId())
                     {
-                        mTextView1.setText(str0+str1+plus+str2+plus+str3+result);
+                        /* 把mRadio1的内容传到mTextView1 */
+                        mTextView1.setText(mRadio1.getText());
                     }
-                    else if(mCheckBox1.isChecked() & mCheckBox2.isChecked() & mCheckBox3.isChecked())
+                    else if(i == mRadio2.getId())
                     {
-                        mTextView1.setText(str0+str2+plus+str3+result);
+                        mTextView1.setText(mRadio2.getText());
                     }
-                    else  if(mCheckBox1.isChecked()==true & mCheckBox2.isChecked()==true & mCheckBox3.isChecked()==false)
-                    {
-                        mTextView1.setText(str0+str1+plus+str2+result); }
-                    else if(mCheckBox1.isChecked()==false & mCheckBox2.isChecked()==false & mCheckBox3.isChecked()==true)
-                    {
-                        mTextView1.setText(str0+str3+plus+result2);
-                    }
-                    else if(mCheckBox1.isChecked()==false & mCheckBox2.isChecked()==true & mCheckBox3.isChecked()==false)
-                    {
-                        mTextView1.setText(str0+str2);
-                    }
-                    else if(mCheckBox1.isChecked()==true & mCheckBox2.isChecked()==false & mCheckBox3.isChecked()==false)
-                    {
-                        mTextView1.setText(str0+str1);
-                    }
-                    else if(mCheckBox1.isChecked()==false & mCheckBox2.isChecked()==false & mCheckBox3.isChecked()==false)
-                    {
-                        mTextView1.setText(str0);
-                    }
-
-                }
-            };
-
-
-    private CheckBox.OnTouchListener mCheckBoxTouch =
-            new CheckBox.OnTouchListener()
-            {
-                @Override
-                public boolean onTouch(View view, MotionEvent motionEvent) {
-                    //TODO Auto-generated method stub
-                    /* 判断在触控笔指压此控件的状态*/
-                    if(mCheckBox1.isChecked() == false)
-                    {
-                        /* 当触控笔放开后的动作 */
-                    }
-                    else if( mCheckBox1.isChecked() == true)
-                    {
-                        /* 当触控笔压下后的动作 */
-                    }
-                    return false;
                 }
             };
 
